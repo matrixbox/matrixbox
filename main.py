5c8378b6a9158e5a9bbfcf5ba4d6758e211b0edf
alex-t-84 <146389044+alex-t-84@users.noreply.github.com>
2025-06-10 21:11:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ def next_program_in_list(run=False):
     print(load_settings.installed_apps_list)
     load_settings.installed_apps_list.append(load_settings.installed_apps_list[0])
     load_settings.installed_apps_list.pop(0)
-    pprint(load_settings.installed_apps_list[0] + "               ", line=4, color="yellow")
+    pprint(load_settings.installed_apps_list[0] + "               ", line=4, color="yellow", clear=True)
 
 autostart = settings["autostart"]
 wifi.radio.tx_power = settings["wifi_power"]
