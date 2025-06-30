a816077c12c0bd1ab79aab3cb8999af764095bce
matrixbox <info@t-skylt.se>
2025-06-30 20:17:11 +0200

    Add files via upload

--- scroller/code.py
+++ scroller/code.py
@@ -4,6 +4,9 @@ from __main__ import *
 exit = False
 start_x = 300
 padding_length = 30
+default_offset = 0
+default_scale = 1
+
 with open("scroller.html") as f: html = f.read()
 
 
@@ -19,21 +22,24 @@ def webinterface(request):
 
 @ampule.route("/", method="POST")
 def scroller_webinterface_port(request):
-    global scroller_text, exit
+    global scroller_text, exit, default_offset, default_scale
     print("POSTED")
     print(request.params)
     print(request.body)
-    if "text" in request.params: scroller_text = "        " + request.params["text"].replace("%20", " ") + "        "
+    if "text" in request.params: scroller_text = "        " + url_decoder(request.params["text"].replace("%20", " ") + "        ")
     if "size" in request.params: 
         if request.params["size"] == "mini": load_screen.currentfont = font_mini
         if request.params["size"] == "small": load_screen.currentfont = font_small
         if request.params["size"] == "large": load_screen.currentfont = font_large
     #if "exit" in request.params: exit = True
     if "color" in request.params: load_screen.currentcolor = request.params["color"]
+    if "offset" in request.params: default_offset = int(request.params["offset"])
+    if "scale" in request.params: default_scale = int(request.params["scale"])
     if "url" in request.body: 
         print("URL found")
         print(request.body)
     set_text()
+    return (200, {}, "OK")
 
 @ampule.route("/", method="GET")
 def scroller_webinterface(request):
@@ -47,14 +53,14 @@ clearscreen()
 
 def padding(scroller_text): return "("*padding_length + scroller_text + "("*padding_length
 def set_length(scroller_width):
-    global window
+    global window, default_scale
     window = displayio.Bitmap(scroller_width, 32, 10)
     scroller_window = displayio.TileGrid(window, pixel_shader=palette)
-    scroller_screen = displayio.Group(scale=2)
+    scroller_screen = displayio.Group(scale=default_scale)
     scroller_screen.append(scroller_window)
     display.root_group = scroller_screen
     scroller_window.x = start_x
-    microcontroller.cpu.frequency = 160000000
+    microcontroller.cpu.frequency = 240000000
     if settings["width"] == 192: microcontroller.cpu.frequency = 240000000
     if settings["height"] == 64: microcontroller.cpu.frequency = 240000000
     return scroller_window
@@ -70,9 +76,11 @@ set_text()
 
 while not exit:
     ampule.listen(socket)
+    scroller_window.y = default_offset
     if scroller_window.x == -scroller_width + padding_length: scroller_window.x = start_x
     scroller_window.x -= 1
     refresh()
+    #print(default_offset)
     b = check_if_button_pressed()
     if b == 2: sys.exit()
         
