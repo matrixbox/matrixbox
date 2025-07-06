ca8608f8f5e30063d46dfee5486525df213c5f9d
matrixbox <info@t-skylt.se>
2025-07-06 23:26:18 +0200

    Add files via upload

--- scroller/code.py
+++ scroller/code.py
@@ -6,6 +6,8 @@ start_x = 300
 padding_length = 30
 default_offset = 0
 default_scale = 1
+scroll = 1
+load_screen.currentfont = font_large
 btc = 0
 timer = time.monotonic()
 
@@ -24,12 +26,15 @@ def webinterface(request):
 
 @ampule.route("/", method="POST")
 def scroller_webinterface_port(request):
-    global scroller_text, exit, default_offset, default_scale, btc
+    global scroller_text, exit, default_offset, default_scale, btc, scroll, scroller_window, padding_length
     print("POSTED")
     print(request.params)
     print(request.body)
-    if "btc" in request.params: 
-        btc = 1 - btc
+    if "btc" in request.params: btc = 1 - btc
+    if "scroll" in request.params: 
+        scroll = 1 - scroll
+        scroller_window.x = 0-(padding_length)
+        return (200, {}, "OK")
     if "text" in request.params: scroller_text = "        " + url_decoder(request.params["text"].replace("%20", " ") + "        ")
     if "size" in request.params: 
         if request.params["size"] == "mini": load_screen.currentfont = font_mini
@@ -42,9 +47,9 @@ def scroller_webinterface_port(request):
     if "url" in request.body: 
         print("URL found")
         print(request.body)
-    set_text()
-
+    
     if btc: set_btc()
+    else: set_text()
 
     return (200, {}, "OK")
 
@@ -67,7 +72,7 @@ def set_length(scroller_width):
     scroller_screen.append(scroller_window)
     display.root_group = scroller_screen
     scroller_window.x = start_x
-    microcontroller.cpu.frequency = 240000000
+    microcontroller.cpu.frequency = 160000000
     if settings["width"] == 192: microcontroller.cpu.frequency = 240000000
     if settings["height"] == 64: microcontroller.cpu.frequency = 240000000
     return scroller_window
@@ -97,17 +102,24 @@ set_text()
 while not exit:
     ampule.listen(socket)
     scroller_window.y = default_offset
-    if scroller_window.x == -scroller_width + padding_length: 
+    if scroll:
+        if scroller_window.x == -scroller_width + padding_length: 
+            if btc and time.monotonic() > timer + 30: 
+                timer = time.monotonic()
+                set_btc()
+            scroller_window.x = start_x
+        scroller_window.x -= 1
+    else:
         if btc and time.monotonic() > timer + 30: 
             timer = time.monotonic()
             set_btc()
-        scroller_window.x = start_x
-    scroller_window.x -= 1
+            scroller_window.x = 0-(padding_length)
+        
     refresh()
     b = check_if_button_pressed()
     if b == 2: sys.exit()
 
         
  
-scroller_window.x = 0
+
 clearscreen()
