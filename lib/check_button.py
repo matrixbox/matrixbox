21264f475ae55ba32a438a2dcff00001fd9ae34f
matrixbox <info@t-skylt.se>
2026-04-04 16:23:08 +0200

    Add files via upload

--- lib/check_button.py
+++ lib/check_button.py
@@ -3,16 +3,45 @@ import digitalio, board
 def time_button():
     return 1 - button.value
 
+_btn_state = 0  # 0=IDLE, 1=WAIT_DEBOUNCE, 2=WAIT_LONG, 3=WAIT_RELEASE
+_btn_time = 0
+
 def check_if_button_pressed():
-    z=0
-    #time.sleep(debounce_delay)
-    if time_button():
-        time.sleep(debounce_delay)
-        if time_button():
-            time.sleep(debounce_delay*2)
-            return 2
-        return 1
-    return z
+    global _btn_state, _btn_time
+    now = time.monotonic()
+    pressed = time_button()
+
+    if _btn_state == 0:
+        if pressed:
+            _btn_time = now
+            _btn_state = 1
+        return 0
+
+    if _btn_state == 1:
+        if now - _btn_time < debounce_delay:
+            return 0
+        if pressed:
+            _btn_state = 2
+        else:
+            _btn_state = 0
+            return 1
+        return 0
+
+    if _btn_state == 2:
+        if now - _btn_time < debounce_delay * 3:
+            if not pressed:
+                _btn_state = 0
+                return 1
+            return 0
+        _btn_state = 3
+        return 2
+
+    if _btn_state == 3:
+        if not pressed:
+            _btn_state = 0
+        return 0
+
+    return 0
     
     
 
